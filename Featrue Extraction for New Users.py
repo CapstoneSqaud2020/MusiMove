ff8ab07dc34eaacfcf8d6b40ded9b514352047fb
AishwaryaNair97 <55207831+AishwaryaNair97@users.noreply.github.com>
2020-03-04 17:33:56 -0500

    Add files via upload
    .py version

---
+++ Featrue Extraction for New Users.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def findGEI(preProcessedData):
+    
+    GEIs = []
+    testDf = pd.DataFrame()
+    
+    testDf = pd.DataFrame.from_dict(preProcessedData, dtype = 'uint8')
+    
+    for d in preProcessedData:
+        tempArr = np.asarray(d.get("cycleImgs"))
+        GEIs.append(np.mean(tempArr, axis = 0))
+        
+    subIds = pd.DataFrame.to_numpy(testDf['usr_id'], dtype = 'int32')
+    
+    return GEIs, subIds
+
+
+# In[2]:
+
+
+from sklearn.model_selection import train_test_split
+
+def getFeatures(preProcessedData):
+    
+    combinedGEIs, subIds = findGEI(preProcessedData)
+    
+    X_train, X_test, y_train, y_test = train_test_split(combinedGEIs, subIds, test_size=0.30, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
+
+# In[3]:
+
+
+def callModel(preProcessedData):
+    
+    X_train, X_test, y_train, y_test = getFeatures(preProcessedData)
+    
+    probVectorORClass = CNN(X_train, X_test, y_train, y_test) #final list of probabilities/final classification
+    
+    return probVectorORClass
+    
+
