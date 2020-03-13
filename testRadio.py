fd855546647ce40140ddbab3eeafb2eba774bad3
AishwaryaNair97 <55207831+AishwaryaNair97@users.noreply.github.com>
2020-03-13 10:25:00 -0400

    Add files via upload
    updated offline playing

--- testRadio.py
+++ testRadio.py
@@ -1,27 +1,38 @@
 import vlc
 import time
-
+import os
+from mutagen.mp3 import MP3
 
 player = None
 is_paused = True
 
 def setRadio(url):
-    global player 
+    global player
     player= vlc.MediaPlayer(url)
 
 def setPlaylist():
     global player
+    musicFolder = "C:\\Users\\aishw\\Desktop\\Capstone Stuff\\mpSongs"
+    songs = os.listdir(musicFolder)
 
+    for x in range(len(songs)):
+        song = MP3(musicFolder + "\\" + songs[x])
+        songLength = song.info.length
+
+        os.startfile(musicFolder + "\\" + songs[x])
+
+        time.sleep(songLength)
+        is_paused = False
 
 def play():
-    global player, is_paused 
+    global player, is_paused
     if not(player is None) and is_paused:
         player.play()
         is_paused = False
 
 def pause():
 
-    global player, is_paused 
+    global player, is_paused
     if not (player is None and is_paused):
         player.pause()
         is_paused = True
@@ -31,5 +42,3 @@ def stop():
     if not(player is None):
         player.stop()
     is_paused = True
-
-
